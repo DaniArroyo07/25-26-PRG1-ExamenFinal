6206a259ff73b88ac33665884c714e64463c5974
DaniArroyo07 <daniel.arroyo@alumnos.uneatlantico.es>
2026-01-09 15:35:14 +0100

    Información añadida
    Añado toda la información que encontrábamos en  el "main" y debíamos meter en el método estático (Estadísticas, Características, Nº palabras).

--- entregas/ArroyoSanchezDaniel/AnalizadorTexto.java
+++ entregas/ArroyoSanchezDaniel/AnalizadorTexto.java
@@ -37,33 +37,7 @@ class AnalizadorTexto {
             }
         }
 
-        System.out.println("ESTADÍSTICAS BÁSICAS");
-        System.out.println("Longitud: " + textoUsuario.length());
-        System.out.println("Vocales: " + vocales);
-        System.out.println("Consonantes: " + consonantes);
-        System.out.println("Números: " + numeros);
-        System.out.println("Espacios: " + espacios);
-        System.out.println("Otros: " + otros);
-
-        if (vocales > consonantes && consonantes > 0 && textoUsuario.length() > 20 && espacios >= 2 && numeros == 0) {
-            System.out.println("Categoría: Texto literario con alta densidad vocálica");
-        } else if (consonantes > vocales && vocales > 0 && textoUsuario.length() > 15 && (otros >= 3 || numeros >= 2) && espacios >= 1) {
-            System.out.println("Categoría: Texto técnico o científico");
-        } else if (numeros > 5 && (vocales + consonantes) < textoUsuario.length() / 2 && espacios < textoUsuario.length() / 10 && otros >= 2) {
-            System.out.println("Categoría: Código o expresión matemática");
-        } else if (espacios > textoUsuario.length() / 5 && vocales > 0 && consonantes > 0 && numeros < 3 && otros < vocales / 2) {
-            System.out.println("Categoría: Texto informal o conversacional");
-        } else {
-            System.out.println("Categoría: Texto genérico");
-        }
-
-        int palabras = 1;
-        for (int i = 0; i < textoUsuario.length(); i++) {
-            if (textoUsuario.charAt(i) == ' ' && i < textoUsuario.length() - 1 && textoUsuario.charAt(i + 1) != ' ') {
-                palabras++;
-            }
-        }
-        System.out.println("Palabras estimadas: " + palabras);
+        
 
         double densidadVocales = (vocales * 100.0) / textoUsuario.length();
         if (densidadVocales > 45 && vocales > 10 && consonantes > 5 && espacios > 2) {
@@ -94,9 +68,36 @@ class AnalizadorTexto {
     }
 
     static void CaracteristicasYEstadisticas (String textoUsuario, int vocales, int consonantes, int numeros, int espacios, int otros) {
-        
+        System.out.println("ESTADÍSTICAS BÁSICAS");
+        System.out.println("Longitud: " + textoUsuario.length());
+        System.out.println("Vocales: " + vocales);
+        System.out.println("Consonantes: " + consonantes);
+        System.out.println("Números: " + numeros);
+        System.out.println("Espacios: " + espacios);
+        System.out.println("Otros: " + otros);
+
+        if (vocales > consonantes && consonantes > 0 && textoUsuario.length() > 20 && espacios >= 2 && numeros == 0) {
+            System.out.println("Categoría: Texto literario con alta densidad vocálica");
+        } else if (consonantes > vocales && vocales > 0 && textoUsuario.length() > 15 && (otros >= 3 || numeros >= 2) && espacios >= 1) {
+            System.out.println("Categoría: Texto técnico o científico");
+        } else if (numeros > 5 && (vocales + consonantes) < textoUsuario.length() / 2 && espacios < textoUsuario.length() / 10 && otros >= 2) {
+            System.out.println("Categoría: Código o expresión matemática");
+        } else if (espacios > textoUsuario.length() / 5 && vocales > 0 && consonantes > 0 && numeros < 3 && otros < vocales / 2) {
+            System.out.println("Categoría: Texto informal o conversacional");
+        } else {
+            System.out.println("Categoría: Texto genérico");
+        }
+
+        int palabras = 1;
+        for (int i = 0; i < textoUsuario.length(); i++) {
+            if (textoUsuario.charAt(i) == ' ' && i < textoUsuario.length() - 1 && textoUsuario.charAt(i + 1) != ' ') {
+                palabras++;
+            }
+        }
+        System.out.println("Palabras estimadas: " + palabras);
     }
 
 }
+
 
 
